bea269566e3b60269126e7abb307b915ee59614b
Deepali Yadav <gauriy473@gmail.com>
2024-06-01 14:37:54 +0530

    Add files via upload

---
+++ Day 017 - For Loops/main.py
@@ -0,0 +1,24 @@
+# Iterating over a string
+name = "Yash"
+for i in name:
+  print(i)
+  if(i=="a"):
+    print("This is something special!")
+
+# Iterating over a list
+colors = ["Red, Blue, Green, Yellow"]
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)
+
+# Range function
+for d in range(5):
+  print(d+1)
+
+for e in range(1, 7):
+  print(e)
+
+for k in range(1,15,9):
+  print(k)
+  
---
+++ Day 018 - While Loops/main.py
@@ -0,0 +1,11 @@
+# While loop
+i = 1
+while(i<5):
+  print(i)
+  i = i+1
+
+# Decrementing while loop
+count =5
+while(count > 0):
+  print(count)
+  count = count - 1
